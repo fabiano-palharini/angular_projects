bab44d9cb8e29dfc3867b4dfd885278d9c5dcc2e
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-01-24 13:59:26 -0300

    menus lists and few screens

--- my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Ribs','This is a test','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('Churrasco','This is a test','https://www.grillsportverein.de/grillrezepte/cache/recipe/354x280/3597.png?1507503470')
+  ];
 
   constructor() { }
 
---
+++ my-recipes/src/app/recipes/recipe-list/recipe.model.ts
@@ -0,0 +1,13 @@
+export class Recipe {
+  private name: string;
+  private desc: string;
+  private imagePath: string;
+
+  constructor(name: string, desc: string, imagePath: string) {
+    this.name = name;
+    this.desc = desc;
+    this.imagePath = imagePath;
+  }
+
+
+}
