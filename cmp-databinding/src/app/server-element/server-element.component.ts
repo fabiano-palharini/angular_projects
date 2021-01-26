40f9fbad5707430804641d12c53245d213d97070
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-01-26 18:40:32 -0300

    ContentChild

--- cmp-databinding/src/app/server-element/server-element.component.ts
+++ cmp-databinding/src/app/server-element/server-element.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ContentChild, ElementRef, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, AfterContentInit {
   @Input('srvElement') element : {type: string, name: string, content: string};
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log('ngOnInit called!');
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called!');
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
+  }
 }
