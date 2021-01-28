cfc3371c9d9a116204e9d2e2fa3d0cc44dad89db
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-01-28 06:40:27 -0300

    adding navigation event on menu items

--- my-recipes/src/app/header/header.component.ts
+++ my-recipes/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() clickedFeature: EventEmitter<string> = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipesClick(featureName: string) {
+    this.clickedFeature.emit(featureName);
+  }
+
+  onShoppingListClick(featureName: string) {
+    this.clickedFeature.emit(featureName);
+  }
 }
