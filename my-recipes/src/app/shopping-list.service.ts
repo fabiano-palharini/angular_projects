c81b8ddf3056a216dd3843c7d1e51bcb6e62c9ee
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-02-04 07:36:23 -0300

    refactor of shopping list using service

---
+++ my-recipes/src/app/shopping-list.service.ts
@@ -0,0 +1,20 @@
+import { Ingredient } from './shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
+
+export class ShoppingListService {
+  ingredientAdded = new EventEmitter<Ingredient[]>();
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('apples', 5),
+    new Ingredient('tomatoes', 10)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientAdded.emit(this.ingredients);
+  }
+}
