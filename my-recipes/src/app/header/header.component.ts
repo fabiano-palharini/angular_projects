df99ec14797cb0320d14100e87097c7a7f15393a
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-02-11 07:56:22 -0300

    refactoring navigation by using routes

--- my-recipes/src/app/header/header.component.ts
+++ my-recipes/src/app/header/header.component.ts
@@ -6,15 +6,8 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  @Output() clickedFeature: EventEmitter<string> = new EventEmitter<string>();
-
   constructor() { }
 
   ngOnInit(): void {
   }
-
-  onSelected(featureName: string) {
-    this.clickedFeature.emit(featureName);
-  }
-
 }
