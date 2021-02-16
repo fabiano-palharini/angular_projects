488d9d06a6c59abb46d5db2c65b500d42727975d
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-02-16 08:21:31 -0300

    rxjs.interval, rxjs.Subscription, unsubscribe

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  firstSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    this.firstSubscription = interval(1000).subscribe(
+                                (counter: number) => {
+                                  console.log(counter);
+                                }
+                              );
+  }
+
+  ngOnDestroy() {
+    this.firstSubscription.unsubscribe();
   }
 
 }
