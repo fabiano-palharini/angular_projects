c47677040daa5e8c4ec95f546ff3055d58b1fd08
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-02-20 16:49:13 -0300

    ngSubmit, ngModel, ngForm simple example

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 
+class User {
+  username: string;
+  email: string;
+  secret: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  user: User = new User();
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: HTMLFormElement) {
+    console.log(form);
   }
 }
