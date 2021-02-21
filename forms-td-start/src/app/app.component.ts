695ca369524ad0c39502825da685ef7b9d7dc637
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-02-21 09:05:00 -0300

    using data form sample

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -17,7 +17,14 @@ export class AppComponent {
   defaultQuestion: string = 'pet';
   answer: string;
   genders = ['male', 'female'];
-  // user: User = new User();
+  submitted = false;
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender:''
+  };
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -38,7 +45,12 @@ export class AppComponent {
   }
 
   onSubmit() {
-    console.log(this.signUpForm);
+    this.submitted = true;
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
   }
 
   // onSubmit(form: HTMLFormElement) {
