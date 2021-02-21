054bf85d08575edcefa70a4cd7b3f53757ebebdc
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-02-21 15:44:00 -0300

    setValue, patchValue, reset for reactive forms

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -21,10 +21,34 @@ export class AppComponent implements OnInit {
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
+
+    this.signUpForm.valueChanges.subscribe(
+      (value) => console.log(value)
+    );
+
+    this.signUpForm.statusChanges.subscribe(
+      (status) => console.log(status)
+    );
+
+    this.signUpForm.setValue({
+      'userData': {
+        'username': 'Max',
+        'email': 'max@test.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
+
+    this.signUpForm.patchValue({
+      'userData': {
+        'username': 'Neno'
+      }
+    });
   }
 
   onSubmit() {
     console.log(this.signUpForm);
+    this.signUpForm.reset();
   }
 
   onAddHobby() {
