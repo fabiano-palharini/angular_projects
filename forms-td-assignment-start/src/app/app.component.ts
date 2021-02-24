cfdabcf52d706dc4451ce0dac7fc4bef19988817
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-02-24 18:03:04 -0300

    solution for the assignment

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') myForm: NgForm;
+  defaultSubscription: string = 'advanced';
+  email: string;
+  subscription: string;
+  password: string;
+
+  onSubmit() {
+    this.email = this.myForm.value.email;
+    this.subscription = this.myForm.value.subscription;
+    this.password = this.myForm.value.password;
+  }
 
 }
