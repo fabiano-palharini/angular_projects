331c826d72029bcd15558a3862d728743a47ed8a
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-02-26 08:03:50 -0300

    reactive form with custom validator

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+  forbiddenNames = ['Test'];
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectData': new FormGroup({
+        'name': new FormControl(null, [Validators.required, this.forbiddenProjectNames.bind(this)]),
+        'email': new FormControl(null, Validators.email),
+        'status': new FormControl(null, Validators.required),
+      })
+    })
+  }
+
+  forbiddenProjectNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenNames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true}
+    }
+    return null;
+  }
 }
