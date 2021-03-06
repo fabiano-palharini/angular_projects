0790ddad1eb9c19e00680661afc037f2a76d73cf
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-03-06 15:26:04 -0300

    custom pipe

---
+++ pipes-start/src/app/shorten.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'shorten'
+})
+export class ShortenPipe implements PipeTransform {
+
+  // transform(value: unknown, ...args: unknown[]): unknown {
+  //   return null;
+  // }
+  transform(value: any) {
+    if (value.length > 10) {
+      return value.substr(0, 10) + '...';
+    }
+
+    return value.substr(0, 10);
+  }
+
+}
