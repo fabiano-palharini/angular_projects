a9137b7596afe351eb1b9356f99ab4ae50c20ecb
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-03-07 16:12:27 -0300

    refactoring http genericmethods

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -19,7 +19,9 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: Post) {
     //the .json below is a firebase requirement.
-    this.http.post('https://my-test-project-9ddf9-default-rtdb.firebaseio.com/posts.json', postData).subscribe(
+    this.http
+    .post<{name: string}>('https://my-test-project-9ddf9-default-rtdb.firebaseio.com/posts.json', postData)
+    .subscribe(
       (responseData) => {
         console.log(responseData);
       }
@@ -36,9 +38,9 @@ export class AppComponent implements OnInit {
 
   private fetchPosts()  {
     this.http
-      .get('https://my-test-project-9ddf9-default-rtdb.firebaseio.com/posts.json')
+      .get<{[key: string]: Post}>('https://my-test-project-9ddf9-default-rtdb.firebaseio.com/posts.json')
       .pipe(
-        map((responseData: {[key: string]: Post}) => {
+        map((responseData) => {
           const postsArray: Post[] = [];
           for (const key in responseData) {
             if (responseData.hasOwnProperty(key)) {
