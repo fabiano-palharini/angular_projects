333ae51fbca253d3e57a1d29439a2351459a0c48
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-03-07 16:35:19 -0300

    loading message

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching: boolean = false;
 
   constructor(private http: HttpClient) {}
 
@@ -37,6 +38,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts()  {
+    this.isFetching = true;
     this.http
       .get<{[key: string]: Post}>('https://my-test-project-9ddf9-default-rtdb.firebaseio.com/posts.json')
       .pipe(
@@ -52,6 +54,7 @@ export class AppComponent implements OnInit {
       )
       .subscribe(
         posts => {
+          this.isFetching = false;
           this.loadedPosts = posts;
         }
     );
