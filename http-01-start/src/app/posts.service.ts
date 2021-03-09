0db45571d4e969cf761466e332778c7c97515034
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-03-09 18:14:14 -0300

    observing responses

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpEventType } from '@angular/common/http';
 import { Post } from './post.model';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
@@ -12,7 +12,7 @@ export class PostsService {
 
   createAndStorePost(postData: Post) {
     this.http
-      .post<{name: string}>('https://my-test-project-9ddf9-default-rtdb.firebaseio.com/posts.json', postData)
+      .post<{name: string}>('https://my-test-project-9ddf9-default-rtdb.firebaseio.com/posts.json', postData, {observe:'response'})
       .subscribe(
         (responseData) => {
           console.log(responseData);
@@ -49,6 +49,19 @@ export class PostsService {
   }
 
   deletePosts(){
-    return this.http.delete('https://my-test-project-9ddf9-default-rtdb.firebaseio.com/posts.json');
+    return this.http
+      .delete('https://my-test-project-9ddf9-default-rtdb.firebaseio.com/posts.json', {observe: 'events'})
+      .pipe(
+        tap(event => {
+          console.log(event);
+          if (event.type === HttpEventType.Sent) {
+            console.log('Request sent over the wire');
+          }
+          if (event.type === HttpEventType.Response) {
+            console.log('Response including the body was received');
+            console.log(event.body);
+          }
+        })
+      );
   }
 }
