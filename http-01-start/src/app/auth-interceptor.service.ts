3f4e0e9b30b2978c4278375788e627af6adeac7a
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-03-12 07:05:28 -0300

    manipulating request and response in the interceptor

--- http-01-start/src/app/auth-interceptor.service.ts
+++ http-01-start/src/app/auth-interceptor.service.ts
@@ -1,12 +1,26 @@
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpEventType } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     // req.url
-    console.log('request is on its way!');
-    return next.handle(req); //next.handle is a method that allows the request to continue its journey
+    console.log(req.url);
+    console.log('request ' + req.url + ' is on its way!');
+    const modifiedRequest = req.clone({
+      headers: req.headers.append('TEST', 'xyz')
+    });
+    // return next.handle(req); //next.handle is a method that allows the request to continue its journey
+    return next.handle(modifiedRequest).pipe(
+      tap( event => {
+        console.log(event);
+        if (event.type === HttpEventType.Response) {
+          console.log('REsponse arrived, body data: ');
+          console.log(event.body);
+        }
+      })
+    );
   }
 
 }
