11b8864c8bf9ff2b2e1b8246df5f8dd14463278e
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-03-13 18:14:33 -0300

    bug fix for recipe list subscription

--- my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/cor
 import { Recipe } from './recipe.model';
 import { RecipeService } from 'src/app/recipe.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
@@ -11,6 +12,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class RecipeListComponent implements OnInit, OnDestroy {
   // @Output() recipeWasSelected: EventEmitter<Recipe> = new EventEmitter<Recipe>();
   recipes: Recipe[];
+  recipesSubscription: Subscription;
   // recipes: Recipe[] = [
   //   new Recipe('Ribs','This is a test','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
   //   new Recipe('Churrasco','This is a test','https://www.grillsportverein.de/grillrezepte/cache/recipe/354x280/3597.png?1507503470')
@@ -19,16 +21,16 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.recipeService.recipesChanged.subscribe(
-      (recipes: Recipe[]) => {
-        this.recipes = recipes;
-      }
-    );
+    this.recipesSubscription = this.recipeService.recipesChanged.subscribe(
+                                  (recipes: Recipe[]) => {
+                                    this.recipes = recipes;
+                                  }
+                                );
     this.recipes = this.recipeService.getRecipes();
   }
 
   ngOnDestroy(): void {
-    this.recipeService.recipesChanged.unsubscribe();
+    this.recipesSubscription.unsubscribe();
   }
 
   // onRecipeSelected(recipe: Recipe) {
