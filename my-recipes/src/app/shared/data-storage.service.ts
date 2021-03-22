2f4983c72aaa3f5b39a24524d5629ea8b35831bc
Palharini, Fabiano <Fabiano_Palharini@Dell.com>
2021-03-22 16:35:13 -0300

    attachin a token to an interceptor

--- my-recipes/src/app/shared/data-storage.service.ts
+++ my-recipes/src/app/shared/data-storage.service.ts
@@ -21,20 +21,16 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap(user => {
-        return this.http.get<Recipe[]>(this.dbUrl, {params: new HttpParams().set('auth', user.token)});
-      }),
-      map(recipes => {
-          return recipes.map(recipe => {
-            return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-          });
-      }),
-      tap(recipes => {
-        this.recipeService.setRecipes(recipes);
-      })
-    );
-
+    return this.http.get<Recipe[]>(this.dbUrl)
+      .pipe(
+        map(recipes => {
+            return recipes.map(recipe => {
+              return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+            });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
